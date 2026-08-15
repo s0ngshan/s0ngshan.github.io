46982b6471a7307784daa3a12e5649df3010f546
s0ngshan <1773728852@qq.com>
2026-08-15 19:34:22 +0800

    Site updated: 2026-08-15 19:34:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/30/个人博客搭建（Hexo-anzhiyu）/","2026/07/30/160CrackMe-1/"];function toRandomPost(){
+var posts=["2026/08/15/IoT逆向入门之固件分析/","2026/07/30/个人博客搭建（Hexo-anzhiyu）/","2026/07/30/160CrackMe-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
